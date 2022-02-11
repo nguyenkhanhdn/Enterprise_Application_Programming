aecaf20efb5f1ae1829693167802ceda94cd809d
Khanh Nguyen <khanhdsp@gmail.com>
2022-02-11 18:49:09 +0700

    Add API Enable Cors

---
+++ WebAPICORS_API/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+namespace WebAPICORS_API.Controllers
+{
+    [EnableCors(origins: "https://localhost:44321", headers: "*", methods: "*")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("GET: Test message")
+            };
+        }
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("POST: Test message")
+            };
+        }
+        [HttpPut]
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("PUT: Test message")
+            };
+        }
+    }
+}
---
+++ WebAPICORS_Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPICORS_Client.MvcApplication" Language="C#" %>
